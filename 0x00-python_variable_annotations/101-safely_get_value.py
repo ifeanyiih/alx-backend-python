5138c61776384a0f1a601649c8565f4b85bd30b3
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-06 19:13:16 +0000

    update

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""More involved type annotations"""
+"""More involved type annotations. Adding the correct
+anotations to function. If it looks like a Duck
+"""
 from typing import Union, Any, Mapping, TypeVar
 
 
@@ -9,6 +11,12 @@ T = TypeVar('T')
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None):
     """Given the parameters and the return values,
     add type annotations to the function
+    Args:
+        dct (dictionary): dct
+        key: (unknown): key
+        default: (specified | None): default
+    Returns:
+        dictionary item or None
     """
     if key in dct:
         return dct[key]

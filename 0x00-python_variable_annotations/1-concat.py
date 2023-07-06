486e9d14cb122210ecd739e532d58b33849047b6
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-06 13:50:04 +0000

    complete task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function concat that takes a
+string str1 and a string str2 as arguments and returns a
+concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function takes two strins and returns the
+    strings concatenated
+    Args:
+        str1 (str): str1
+        str2 (str): str2
+    Returns:
+        str : a concatenated string
+    """
+    return str1 + str2

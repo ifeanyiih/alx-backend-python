38ef4d6bd78bca098a28ded16b0f2cf9cdd2299c
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-06 14:05:33 +0000

    complete task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Define and annotate variables with specified values:
+a, an integer with value of 1
+pi, float with the value of 3.14
+i_understand_annotations, a boolean with value True
+school a string with value of Holberton
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"

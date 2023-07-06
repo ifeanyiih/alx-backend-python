adf2457e8926811bb934882a90090fb15f737e05
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-06 13:59:05 +0000

    complete task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function to_str that takes a
+float n as argument and returns the string
+representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """Function takes a float n and returns
+    the string representation of the float
+    Args:
+        n (float): n
+    Returns:
+        str : a string representation of float
+    """
+    return str(n)

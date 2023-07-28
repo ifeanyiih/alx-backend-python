ac20228ee92dbc429c5c7a020a94ad8c6c4129d3
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-28 22:14:17 +0000

    complete task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,4 +10,12 @@ from parameterized import parameterized
 
 class TestAccessNestedMap(unittest.TestCase):
     """A unittest test case"""
-
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """tests that the method returns what it is
+        supposed to"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)

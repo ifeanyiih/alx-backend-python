8ed56579e38ba1de33b2a0a51fe0459927d4e525
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-28 14:03:06 +0000

    start project

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Script contains a test case TestAccessNestedMap
+class that implements a method to test that
+utils.access_nested_map returns what it is supposed to
+"""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """A unittest test case"""
+
